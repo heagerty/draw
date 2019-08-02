09dceee86c5e795ba6812f64ada101d948d6549f
Casey Heagerty <cheagerty@gmail.com>
2019-08-02 11:31:04 +0200

    clean up code

--- src/index.js
+++ src/index.js
@@ -15,8 +15,6 @@ function Square(props) {
 class Board extends React.Component {
 
 
-
-
     static numberOfRows = number;
     static boardSize = (Board.numberOfRows*Board.numberOfRows);
 
@@ -24,7 +22,6 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(Board.boardSize).fill(null),
-            xIsNext: true,
         };
     }
 
@@ -34,7 +31,6 @@ class Board extends React.Component {
         squares[i] =  'X';
         this.setState({
             squares: squares,
-            xIsNext: !this.state.xIsNext,
         });
     }
 
@@ -45,54 +41,34 @@ class Board extends React.Component {
         />;
     }
 
-    render() {
-        const winner = calculateWinner(this.state.squares);
-        let status = 'Draw something nice';
+    createBoard = () => {
+        let table = []
 
-        }
-
-
-        // const rows = [];
-        // for (var i = 0; i < Board.boardSize; i++) {
-        //     rows.push(i);
-        // }
-
-
-        // let boxes = rows.map(function(row) {
-        //     return "{this.renderSquare(" + row + ")}"
-        // });
-        //let boxes = [];
-        // for (row of rows) {
-        //     boxes.push({this.renderSquare(row)});
-        // }
-
-        ///////////////
-        let createBoard = () => {
-            let table = []
-
-            // Outer loop to create parent
-            for (let i = 0; i < Board.numberOfRows; i++) {
-                let children = []
-                //Inner loop to create children
-                for (let j = 0; j < Board.numberOfRows; j++) {
-                    children.push(this.renderSquare(i*Board.numberOfRows+j))
-                }
-                //Create the parent and add the children
-                table.push(<div className="board-row">{children}</div>)
+        // Outer loop to create parent
+        for (let i = 0; i < Board.numberOfRows; i++) {
+            let children = []
+            //Inner loop to create children
+            for (let j = 0; j < Board.numberOfRows; j++) {
+                children.push(this.renderSquare(i*Board.numberOfRows+j))
             }
-            return table
+            //Create the parent and add the children
+            table.push(<div className="board-row">{children}</div>)
         }
-        ///////////////
+        return table
+    }
+
+    render() {
+
+        const status = 'Draw something nice';
 
         return (
             <div>
                 <div className="status">{status}</div>
-                {createBoard()}
-
-
+                {this.createBoard()}
             </div>
         );
     }
+
 }
 
 class Game extends React.Component {
@@ -103,32 +79,10 @@ class Game extends React.Component {
                     <Board />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
                 </div>
             </div>
         );
     }
-}
-
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
 }
 
 // ========================================
